a6c71b6aac95b3f7b6098b20cc0ba3fd5ea84a17
Chyld Medford <chyld.medford@gmail.com>
2020-07-29 21:59:31 -0700

    mvp finished

--- client/src/Atom.svelte
+++ client/src/Atom.svelte
@@ -1,7 +1,4 @@
 <script>
-  import rough from "roughjs/bundled/rough.cjs.js";
-  import { onMount } from "svelte";
-
   export let questionUrl;
   export let title;
 
@@ -9,46 +6,11 @@
   let guess;
   let questionObject;
   let inputRef;
-  let draw;
-  let rc;
-  let dimW = 250;
-  let dimH = 75;
-
-  onMount(() => {
-    draw = document.getElementById("draw");
-    rc = rough.canvas(draw);
-  });
 
   $: {
     // runs when title changes
     console.log("Loading:", title);
     clickQuestion();
-  }
-
-  function choice(list) {
-    const n = list.length;
-    const i = Math.floor(Math.random() * n);
-    return list[i];
-  }
-
-  function drawSketch() {
-    const colors = ["red", "blue", "green", "purple", "#ff33cc"];
-    const fills = [
-      "hachure",
-      "zigzag",
-      "cross-hatch",
-      "dots",
-      "sunburst",
-      "dashed",
-      "zigzag-line",
-    ];
-    rc.ctx.clearRect(0, 0, dimW, dimH);
-    rc.rectangle(10, 10, dimW - 20, dimH - 20, {
-      roughness: 1.8,
-      fill: choice(colors),
-      fillStyle: choice(fills),
-      roughness: 2.5,
-    });
   }
 
   function displayMath(markup) {
@@ -64,7 +26,6 @@
       questionObject = JSON.parse(text);
       guess = "";
       inputRef.focus();
-      drawSketch();
       return questionObject;
     } else {
       throw new Error(text);
@@ -98,35 +59,35 @@
 </script>
 
 <style>
-  .question {
+  #question {
     color: #ff33cc;
     font-size: 55px;
   }
-  #guess {
-    font-size: 40px;
+  #guessdiv {
+    text-align: center;
     position: relative;
-    top: -80px;
-    background: rgba(255, 255, 255, 0.3);
-    width: 250px;
+    top: 50px;
+  }
+  #guessinput {
+    color: #454545;
     font-family: monospace;
-    color: #353535;
+    font-size: 40px;
+    width: 350px;
   }
 </style>
 
 <div>
-  <canvas id="draw" width={dimW} height={dimH} />
-  <div />
-  <input
-    id="guess"
-    bind:value={guess}
-    on:input={typingAnswer}
-    bind:this={inputRef} />
+  <div id="guessdiv">
+    <input
+      id="guessinput"
+      bind:value={guess}
+      on:input={typingAnswer}
+      bind:this={inputRef} />
+  </div>
 
-  {#await promiseQuestion}
-    <p>...waiting</p>
-  {:then question}
+  {#await promiseQuestion then question}
     {#if question}
-      <div class="question">
+      <div id="question">
         {@html displayMath(question.question)}
       </div>
     {/if}
